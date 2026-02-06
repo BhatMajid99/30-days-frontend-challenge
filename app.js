813d46a7b55d1ef543440f16c4b34e6c191a6be3
BhatMajid99 <majidbhat913@gmail.com>
2026-02-06 22:17:58 +0530

    Day 5: OTP generator with light/dark theme toggle

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-// Sort products by price (low → high)
-let productPrice = [40, 60, 70, 30, 10];
-productPrice.sort((a, b) => a - b);
-console.log(productPrice);
-
-// Filter students who passed (marks > 40)
-let result = document.querySelector("#result");
-let enterBtn = document.querySelector("#enter");
-let marksInput = document.querySelector("#marks");
-
-enterBtn.addEventListener("click", () => {
-    let marks = Number(marksInput.value.trim());
-
-    if (marks <= 0 || marks > 100) {
-        result.innerText = "Invalid input, please enter marks 0–100";
-    } else if (marks > 40) {
-        result.innerText = `Pass, marks = ${marks}`;
-    } else {
-        result.innerText = `Fail, marks = ${marks}`;
+let para = document.querySelector("p");
+let genrateBtn = document.querySelector("#genrateotp");
+let body = document.querySelector("body");
+let themeBtn = document.querySelector("#theme");
+genrateBtn.addEventListener("click", () => {
+    let OTP = Math.floor(Math.random()* 9000) + 1000;
+    para.innerText = `Your OTP is : ${OTP}`;
+})
+let currentTheme = "light";
+themeBtn.addEventListener("click", () =>{
+    if(currentTheme == "light"){
+        body.setAttribute("class", "dark");
+        currentTheme = "dark";
+    }else{
+        body.setAttribute("class", "light");
+        currentTheme = "light";
     }
-});
-
-// Form handling
-let form = document.querySelector("#userForm");
-
-form.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-
-    console.log(form.elements["name"].value);
-    console.log(form.elements["age"].value);
-    console.log(form.elements["gender"].value);
-});
+    
+})
